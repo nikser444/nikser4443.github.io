e19ba9858354dd74afadf09d59fbb38867eea260
nikser444 <pratpienov13@gmail.com>
2025-09-09 23:53:16 +0400

    Delete frontend directory

--- frontend/src/components/auth/index.ts
+++
@@ -1,4 +0,0 @@
-// index.ts - Экспорт всех компонентов авторизации
-export { AuthLayout } from './AuthLayout';
-export { LoginForm } from './LoginForm';
-export { RegisterForm } from './RegisterForm';
--- frontend/src/components/chat/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as ChatList } from './ChatList';
-export { default as ChatWindow } from './ChatWindow';
-export { default as MessageItem } from './MessageItem';
-export { default as MessageInput } from './MessageInput';
-export { default as EmojiPicker } from './EmojiPicker';
